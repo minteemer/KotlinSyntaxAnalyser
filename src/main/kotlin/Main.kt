4c3e86f71d5e1d6e187986fc490f475dc69337f4
Timur <minteemer@gmail.com>
2018-10-14 19:38:59 +0300

    Progress output to console, gradle build script fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+@file:JvmName("Main")
+
 import com.google.gson.GsonBuilder
 import java.io.File
 
@@ -7,8 +9,14 @@ const val OUTPUT_FILE = "out.txt"
 private val gson = GsonBuilder().setPrettyPrinting().create()
 
 fun main(args: Array<String>) {
+    println("Generating tree...")
     val tree = KotlinSyntaxTreeGenerator.generateTree(File(INPUT_FILE).toPath())
 
+    println("Converting to JSON...")
     val output = gson.toJson(tree?.toMap())
+
+    println("Writing output...")
     File(OUTPUT_FILE).writeText(output)
+
+    println("Complete!")
 }
