d50ee9c0f02f14301cf16e31bab21a9f5bb32750
Timur <minteemer@gmail.com>
2018-10-14 20:58:00 +0300

    Tests refactored

--- src/test/kotlin/TreeGeneratorTests.kt
+++ src/test/kotlin/TreeGeneratorTests.kt
@@ -7,23 +7,36 @@ class TreeGeneratorTests {
     companion object {
         private val classLoader = TreeGeneratorTests::class.java.classLoader
 
+        /** Map of test files to expected result files */
+        private lateinit var testFiles: Map<File, File>
 
-        private lateinit var testData: Map<File, File>
+        /** Folder with input .kt files */
+        private val inputFilesFolder = classLoader.getResource("in").file
 
+        /** Folder with expected output .json files */
+        private val outputFilesFolder = classLoader.getResource("out").file
+
+        /**
+         * Sets up [testFiles] map where each file from [inputFilesFolder] mapped on
+         * files in [outputFilesFolder] with the same name, but .json extension.
+         */
         @BeforeAll
         @JvmStatic
-        fun setUpData(){
-            testData = File(classLoader.getResource("in").file).listFiles().toList().associate {
-                it to File(classLoader.getResource("out/${it.name.dropLast(3)}.json").file)
+        fun setUpData() {
+            testFiles = File(inputFilesFolder).listFiles().filter { it.isFile }.associate { input ->
+                input to File("$outputFilesFolder/${input.nameWithoutExtension}.json")
             }
         }
     }
 
-
+    /**
+     * Asserts that syntax trees generated form inputs in [testFiles] is the same
+     * as the trees in output files
+     */
     @TestFactory
-    fun testSquares() = testData
+    fun testTreeGeneration() = testFiles
             .map { (input, output) ->
-                DynamicTest.dynamicTest(input.name) {
+                DynamicTest.dynamicTest(input.nameWithoutExtension) {
                     Assertions.assertEquals(
                             KotlinSyntaxTreeGenerator.generateTree(input.toPath()),
                             output.readText().jsonToMap()
@@ -31,8 +44,10 @@ class TreeGeneratorTests {
                 }
             }
 
-    private val gson = Gson()
-    private val typeToken = object: TypeToken<Map<String, Any>>(){}.type
 
+    private val gson = Gson()
+    private val typeToken = object : TypeToken<Map<String, Any>>() {}.type
+
+    /** @return [HashMap] created from JSON in this string */
     private fun String.jsonToMap(): Map<String, Any> = gson.fromJson(this, typeToken)
 }
--- src/test/resources/in/import.kt
+++ src/test/resources/in/import.kt
@@ -1,1 +1,5 @@
+package test.pack
+
 import org.junit.jupiter.api.Assertions
+
+typealias Строка = Int
